29171cbc6396128f1d86eb8a5386446c6c901551
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-16 15:43:31 +0200

    Main file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <mos/io/window.hpp>
+
+glm::vec2 resolution = glm::vec2(1920, 1080) / 2.0f;
+mos::io::Window window("Skeleton", resolution);
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  window.key_func = [&](int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+      window.close(true);
+    }
+  };
+
+  while (!window.close()) {
+      window.poll_events();
+      window.swap_buffers();
+  }
+
   return 0;
 }
