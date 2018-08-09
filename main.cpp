646f41503279111cf9ff3cacefe97ebe1fd4ced0
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-09 15:15:16 +0200

    Remove.

--- main.cpp
+++ main.cpp
@@ -79,8 +79,8 @@ int main() {
     //scene.models[8].transform = glm::rotate(glm::mat4(1.0f), time, glm::vec3(0.0f, 0.0f, 1.0f)) * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, 0.25f));
 
     auto center = scene.lights[0].center();
-    center.x = glm::sin(time * 0.5f);
-    center.x = -1.0f;
+    //center.x = glm::sin(time * 0.5f);
+    //center.x = -1.0f;
     scene.lights[0].center(center);
 
     gfx_renderer.render({scene}, glm::vec4(0.0f, 0.0f, 0.0, 0.0f), resolution);
